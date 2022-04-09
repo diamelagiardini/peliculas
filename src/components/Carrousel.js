0279b3b2eca223cf53edab9d86db746ed723b355
diamelagiardini <diamelagiardini23@gmail.com>
2022-04-09 20:04:54 -0300

    carrousel

---
+++ src/components/Carrousel.js
@@ -0,0 +1,40 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useEffect, useState } from "react";
+
+const Carrousel = () => {
+  const [PeliculasActuales, setPeliculasActuales] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?api_key=d2db916ed787e45a269779c746706c07&language=en-US"
+    )
+      .then((res) => res.json())
+      .then((data) => setPeliculasActuales(data.results));
+  }, []);
+
+  return (
+    <div className="container">
+      <Slider
+        dots={true}
+        infinite={true}
+        speed={6000}
+        arrows={true}
+        slidesToShow={1}
+        autoplay={true}
+        autoplaySpeed={2000}
+      >
+        {PeliculasActuales.map((pelicula) => (
+          <div className="carrousel-home centrado">
+            <img
+              src={`https://image.tmdb.org/t/p/original/${pelicula.backdrop_path}`}
+              alt=""
+            />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default Carrousel;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FiArrowRight } from "react-icons/fi";
-import Footer from "./Footer"
+import Footer from "./Footer";
+import Carrousel from "./Carrousel";
 
 const Home = () => {
   const [topPeliculas, setTopPeliculas] = useState([]);
@@ -23,34 +24,44 @@ const Home = () => {
   }, []);
   return (
     <>
-    <div className="centrado flex-wrap">
-      <div className="panel-home margen-tarjetas">
-        <div className="titulo-panel">TOP PELICULAS</div>
-        <div>
-          {topPeliculas.map((pelicula) => (
-            <div className="flex space-between items-panel-home">
-              <p>{pelicula.title}</p>
-              <Link to={`/top/${pelicula.id}`}><p className="panel-mas-info"><FiArrowRight/></p></Link>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div></div>
+      <Carrousel />
 
-      <div className="panel-home margen-tarjetas">
-        <div className="titulo-panel">PELICULAS ACTUALES</div>
-        <div>
-          {PeliculasActuales.map((pelicula) => (
-            <div className="flex space-between items-panel-home">
-              <p>{pelicula.title}</p>
-              <Link to={`/actuales/${pelicula.id}`}><p className="panel-mas-info"><FiArrowRight/></p></Link>
-            </div>
-          ))}
+      <div className="componentes-home centrado flex-wrap">
+        <div className="panel-home margen-tarjetas">
+          <div className="titulo-panel">TOP PELICULAS</div>
+          <div>
+            {topPeliculas.map((pelicula) => (
+              <div className="flex space-between items-panel-home">
+                <p>{pelicula.title}</p>
+                <Link to={`/top/${pelicula.id}`}>
+                  <p className="panel-mas-info">
+                    <FiArrowRight />
+                  </p>
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
+        <div></div>
+
+        <div className="panel-home margen-tarjetas">
+          <div className="titulo-panel">PELICULAS ACTUALES</div>
+          <div>
+            {PeliculasActuales.map((pelicula) => (
+              <div className="flex space-between items-panel-home">
+                <p>{pelicula.title}</p>
+                <Link to={`/actuales/${pelicula.id}`}>
+                  <p className="panel-mas-info">
+                    <FiArrowRight />
+                  </p>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div></div>
       </div>
-      <div></div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
   );
 };
